93729f4e2e3ceffb7ad1a0fb52e67036d8f23253
hamsananth <78092120+webpicker606@users.noreply.github.com>
2021-07-01 20:08:29 +0530

    added python file
    uploaded christmas special.py

---
+++ christmas spe 1.py
@@ -0,0 +1,15 @@
+d=1
+def triangle(x,y,n):
+    #x=i,y=j,n
+    while x<=n:
+        print(f" "*y+' *'*x)
+        x=x+2
+        y=y-2
+triangle (1,25,7)
+triangle (5,21,13)
+triangle(11,15,17)
+triangle(15,11,21)
+while d<=4:
+    print (" "*25+"[_]")
+    d=d+1
+
